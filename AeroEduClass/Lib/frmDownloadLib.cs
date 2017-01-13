bc8e93f8207340fbada1897e06e42339183c85dc
gaokaige <gaokaige@aero-edu.com>
2017-01-13 10:59:46 +0800

    上次提交少了一些文件

---
+++ AeroEduClass/Lib/frmDownloadLib.cs
@@ -0,0 +1,90 @@
+﻿using AeroEduClass.Lib.PostgreSQLDB;
+using AeroEduLib;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Windows.Forms;
+
+namespace AeroEduClass.Lib
+{
+    public class frmDownloadLib
+    {
+        IDBHelper dbHelper = new PostgreHelper();
+        string conn = Program._config.ConnectionString;
+        #region 建表
+        
+        /// <summary>
+        /// 如果表不存在则创建
+        /// </summary>
+        /// <param name="tableName"></param>
+        public void IfNotExistCreateTable(string tableName)
+        {
+            string sql = "select count(0) from pg_tables where schemaname='public' and tablename=@TableName";
+
+            string result = dbHelper.ExecuteScalar(conn, CommandType.Text, sql, new Npgsql.NpgsqlParameter("@TableName", tableName)).ToString();
+
+            if (result == "0")
+            {
+                dbHelper.ExecuteNonQuery(conn, CommandType.Text, GetCreateTableSQL(tableName), null);
+            }
+        }
+        /// <summary>
+        /// 根据表名返回建表语句
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        private string GetCreateTableSQL(string tableName)
+        {
+            string sql = string.Empty;
+            switch (tableName)
+            { 
+                case "userdl":
+                    sql = @"CREATE TABLE public.userdl
+                        (
+                           id character(36), 
+                           resource jsonb, 
+                           createdate character varying(40) DEFAULT now(), 
+                           CONSTRAINT userdl_pk_id PRIMARY KEY (id)
+                        ) 
+                        WITH (
+                          OIDS = FALSE
+                        )
+                        ;
+                        ALTER TABLE public.userdl
+                          OWNER TO postgres;";
+                    break;
+                default:
+                    break;
+            }
+            if (string.IsNullOrEmpty(sql))
+                Log.ToFile("建表语句不存在:" + tableName);
+            return sql;
+        }
+
+        #endregion
+
+        #region datagridview operat postgresql
+
+        internal void SaveCompleteItems(DataGridViewRow row)
+        {
+            string sql = "INSERT INTO userdl (id, resource) VALUES";
+            // if insert multi lines
+            //('1', 'json1'),('2', 'json2'),('3', 'json3');";
+            string id = row.Cells["clID"].Value.ToString();
+            JObject jo =new JObject();
+            jo.Add("name", row.Cells["clName"].Value.ToString());
+            jo.Add("path", row.Cells["clPath"].Value.ToString());
+            sql += string.Format("('{0}','{1}');", id, jo.ToString());
+
+            dbHelper.ExecuteNonQuery(conn, CommandType.Text, sql, null);
+        }
+
+        internal DataSet LoadTopLines(int top)
+        {
+            string sql = "select id, resource->>'name' as name,resource->>'path' as path from userdl order by createdate desc limit " + top + ";";
+            DataSet ds = dbHelper.ExecuteQuery(conn, CommandType.Text, sql, null);
+            return ds;
+        }
+
+        #endregion
+    }
+}
---
+++ AeroEduLib/HttpDownloadHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace AeroEduLib
+{
+    /// <summary>
+    /// HTTP下载，有进度、完成、错误事件
+    /// </summary>
+    public class HttpDownloadHandler
+    {
+        public delegate void DownloadProgressHandler(object sender, AeroEventArgs e);
+        public static event DownloadProgressHandler OnProgress;
+
+        public delegate void DownloadCompleteHandler(object sender, AeroEventArgs e);
+        public static event DownloadCompleteHandler OnComplete;
+
+        public delegate void DownloadErrorHandler(object sender, AeroEventArgs e);
+        public static event DownloadErrorHandler OnError;
+        public static bool HttpDownloadFile(string url, string fileName)
+        {
+            Stream stream = null;
+            FileStream fs = null;
+            try
+            {
+                System.Net.HttpWebRequest Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(url);
+                System.Net.HttpWebResponse myrp = (System.Net.HttpWebResponse)Myrq.GetResponse();
+                long totalBytes = myrp.ContentLength;
+
+                stream = myrp.GetResponseStream();
+                fs = new System.IO.FileStream(fileName, System.IO.FileMode.Create);
+                long totalDownloadedByte = 0;
+                byte[] buffer = new byte[1024];
+                int readL = stream.Read(buffer, 0, (int)buffer.Length);
+                // 循环100次报一次进度吧
+                int times = 0;
+                while (readL > 0)
+                {
+                    totalDownloadedByte = readL + totalDownloadedByte;
+                    fs.Write(buffer, 0, readL);
+                    readL = stream.Read(buffer, 0, (int)buffer.Length);
+                    if (times / 100 == 0)
+                        OnProgress(null, new AeroEventArgs((totalDownloadedByte * 100 / totalBytes).ToString()));
+                    times++;
+                }
+                OnProgress(null, new AeroEventArgs("100"));
+                OnComplete(null, new AeroEventArgs("下载完成"));
+            }
+            catch (Exception exc)
+            {
+                OnError(null, new AeroEventArgs(exc.Message));
+                Log.ToFile("下载失败:" + exc);
+                return false;
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                if (fs != null) fs.Close();
+            }
+            return true;
+        }
+
+        public class AeroEventArgs : EventArgs
+        {
+            public string Message { get; set; }
+            public AeroEventArgs(string message)
+            {
+                Message = message;
+            }
+        }
+    }
+}
